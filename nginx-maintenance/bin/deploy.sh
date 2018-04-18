36a427823248697346f1d557057d59115f356fd9
Daegwon Kim <i-am-not-a-machine@nacyot.com>
2018-04-18 17:57:04 +0900

    nginx-maintenance Dockerfile 추가

---
+++ nginx-maintenance/bin/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME="314695318048.dkr.ecr.ap-northeast-2.amazonaws.com/nginx-maintenance:$(git rev-parse --short HEAD)"
+
+$(aws ecr get-login --no-include-email --region ap-northeast-2)
+docker build -t "${IMAGE_NAME}" .
+docker push "${IMAGE_NAME}"
